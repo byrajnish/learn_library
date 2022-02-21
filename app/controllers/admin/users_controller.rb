d4f6918f44346a28f7f49394ff41d4777fab7f9b
rajnish kumar <rajnish@pi314.in>
2022-02-21 23:53:55 +0530

    add librarian form

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,9 @@
 class Admin::UsersController < ApplicationController
     def index
+        @book = Book.all
+        @member = User.all - User.where(user_type: "admin") -User.where(user_type: "librarian")
+        # <%if current_user.present? && current_user.user_type == "admin"%>
+        # @book_avialable = Book.all
     end
 
 end
